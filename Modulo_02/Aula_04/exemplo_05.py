3b3775f9fb127e4693387cafdfae50d94311e502
Rapharosse29 <227596315+rapharosse29@users.noreply.github.com>
2025-11-05 11:26:35 -0300

    Add example of function with type hints
    Introduces a function 'saudar_com_idade' demonstrating Python type hints for parameters. Includes a sample call to the function.

---
+++ Modulo_02/Aula_04/exemplo_05.py
@@ -0,0 +1,7 @@
+# Tipagem de dados
+# Oferece clareza para o código e deixa o código mais limpo.
+
+def saudar_com_idade(nome: str, idade: int):
+    print(f"Olá {nome}! Você tem {idade}.")
+
+saudar_com_idade("Raphael", 34)
