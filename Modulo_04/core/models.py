5a744f4a06efc2457f9e65b99bb1ec50a4e0ef63
Rapharosse29 <227596315+rapharosse29@users.noreply.github.com>
2025-11-17 11:17:56 -0300

    Feat: Foi adicionado o bota de 'Marcar com concluida' e 'Deletar'
    Novos botoes adicionados

--- Modulo_04/core/models.py
+++ Modulo_04/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Tarefa(models.Model):
     titulo = models.CharField(max_length=200)
     concluida = models.BooleanField(default=False)
-    criado_em = models.DateTimeField(auto_now_add=True)
+    criada_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.titulo
