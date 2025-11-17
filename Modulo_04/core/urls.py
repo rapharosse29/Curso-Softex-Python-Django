49a3691f993835ffd5dd256032a7fb31264493dc
Rapharosse29 <227596315+rapharosse29@users.noreply.github.com>
2025-11-17 11:18:37 -0300

    Feat: Adicionadando tarefas
    Funcoes de deletar e criar tarefas

--- Modulo_04/core/urls.py
+++ Modulo_04/core/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home',),
+    path('tarefa/<int:pk>/', views.concluir_tarefa, name='concluir_tarefa',),
+    path('tarefa/<int:pk>/deletar/', views.deletar_tarefa, name='deletar_tarefa'),
+    path('login/', views.login, name='login'),
 ]
